4dcad57eb49cae8c72432d139f274f777f841639
ohmstar123 <opatel22@uwo.ca>
2023-11-02 19:42:10 -0400

    Implemented fetch method to connect front end to back end for partsdb
    The fetch method was added to connect the front and the back end and was tested with the use of the static parts database. This database is there just for understanding purposes and the process will be implemented over to the superhero database in order to send useful information between the server and the client

--- client/script.js
+++ client/script.js
@@ -1,3 +1,55 @@
-function searchByID(){
-    alert("hello")
+document.getElementById('get-inventory').addEventListener('click', getInventory)
+document.getElementById('addItem').addEventListener('click', addInventory)
+
+function getInventory(){
+    fetch("/api/parts")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const list = document.getElementById('inventory')
+
+        // Clear the list
+        while(list.hasChildNodes()){
+            list.removeChild(list.firstChild)
+        }
+
+        // Add each item to the list
+        data.forEach(e => {
+            const item = document.createElement('li')
+            item.appendChild(document.createTextNode(`${e.name} (${e.colour}) Qty: ${e.stock}`))
+            list.appendChild(item)
+        });
+    })
+    
+}
+
+function addInventory(){
+    const newpart = {
+        name : document.getElementById('name').value, 
+        colour : document.getElementById('colour').value, 
+        stock : document.getElementById('stock').value 
+    }
+    console.log(newpart)
+
+    fetch('/api/parts', {
+        method: 'POST', 
+        headers: {'Content-type' : 'application/json'},
+        body: JSON.stringify(newpart)
+    })
+    .then(res => {
+        if (res.ok){
+            res.json()
+            .then(data => {
+                getInventory()
+                console.log(data)
+                document.getElementById('status').innerText = `Created part ${data.id}: ${data.name}`
+            })
+            .catch(err => console.log('Failed to get json object'))
+        }
+        else{
+            console.log('Error: ', res.status)
+            document.getElementById('status').textContent = 'Failed to add item'
+        }
+    })
+    .catch()
 }
